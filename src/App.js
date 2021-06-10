6a0cba8cda8f87e46c6fd14c2c13f5163fc9e9cb
DZ521111 <dhruvkakadiya3@gmail.com>
2021-06-10 08:18:53 +0530

    component added!

--- src/App.js
+++ src/App.js
@@ -1,52 +1,18 @@
 // import logo from './logo.svg';
 import "./App.css";
+import Header from "./my_components/Header";
+import { Todos } from "./my_components/Todos";
+import { Footer } from "./my_components/Footer";
+import { Todo } from "./my_components/Todo";
 
 function App() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            First-App
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Milky-Way
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  About
-                </a>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Find"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav>
+      <Header />
+      <Todos>
+        <Todo />
+      </Todos>
+      <Footer />
     </>
   );
 }
